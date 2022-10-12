d4c8b2c7515a4d53d5cacfa08e9ec9f845efebec
tekkenYo <yaojiannan1985@gmail.com>
2022-10-12 18:09:31 +0900

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  
+  const result = {
+  session: {
+    namespace: 'myNamespace',
+    data: { variable: 'test123' },
+  },
+  messages: [{ type: 'text', config: { textMessages: [{ text: 'random message 1' }] } }],
+  action: {
+    nextDialogstate: 'dialogstate-123-abc',
+  },
+};
+  
+  
+  res.status(200).json(result)
 }
