b1c8713064189552b6afd5961906b3130d1acbf0
tekkenYo <yaojiannan1985@gmail.com>
2023-01-24 14:33:41 +0900

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const body = JSON.parse(req.body)
   const result = {
     proctorGroup: "kimoyo_wa_v1menu_tst",
-    body: body
+    body: JSON.parse(req.body)
   };
   res.status(200).json(result)
 }
