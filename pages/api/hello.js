4c8b8913a9cb9ccb128158f1a4a2a744e258aeec
tekkenYo <yaojiannan1985@gmail.com>
2023-01-24 13:57:28 +0900

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,9 +3,8 @@
 export default function handler(req, res) {
   
   const result = {
-  proctorGroup: "kimoyo_wa_v1menu_tst"
-};
-  
-  
+    proctorGroup: "kimoyo_wa_v1menu_tst"
+  };
+  console.log(req)
   res.status(200).json(result)
 }
