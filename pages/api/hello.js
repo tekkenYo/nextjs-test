51b9a70a8701c6933ed05c5a74ecd5ae07419f04
tekkenYo <yaojiannan1985@gmail.com>
2023-02-14 10:54:04 +0900

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,6 +3,7 @@
 export default function handler(req, res) {
   const result = {
     payload: "{\"userId\": \"user-id-001\"}",
+    payload2: "user-id-002",
     body: req.body
   };
   res.status(200).json(result)
