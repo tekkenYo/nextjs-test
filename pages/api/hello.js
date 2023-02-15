fd4ee0d710fa4cadd984170f01543825e3b119a0
tekkenYo <yaojiannan1985@gmail.com>
2023-02-15 14:40:45 +0900

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
     "body": req.body,
     "name": "Example user", 
     "zip_codes": ["31123", "31125"], 
-    "billing": { "bank": "ABN AMRO" } 
+    "billing": [{ "key": "key_1", "value": "value_1" }, { "key": "key_2", "value": "value_2" }]
   };
   res.status(200).json(result)
 }
